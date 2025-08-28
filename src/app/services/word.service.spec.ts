82457c4c270f2514027be154c1c0dc39c11bae81
Reza Fattahi <rezafatahy@gmail.com>
2025-08-28 15:45:05 +0200

    Added Unit test for word.service

--- src/app/services/word.service.spec.ts
+++ src/app/services/word.service.spec.ts
@@ -13,4 +13,23 @@ describe('WordService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should change umlauts', () => {
+    // LOWERCASE UMLAUTS
+    expect(service.replaceUmlauts('für')).toBe('fuer');
+    expect(service.replaceUmlauts('mädchen')).toBe('maedchen');
+    expect(service.replaceUmlauts('könig')).toBe('koenig');
+    // UPPERCASE UMLAUTS
+    expect(service.replaceUmlauts('Über')).toBe('Ueber');
+    expect(service.replaceUmlauts('Äpfel')).toBe('Aepfel');
+    expect(service.replaceUmlauts('Östereich')).toBe('Oestereich');
+  });
+
+  it("should not change, becouse doesn't included umlaut", () => {
+    expect(service.replaceUmlauts('Hello world')).toBe('Hello world');
+  });
+
+    it("should not change, becouse of empty string", () => {
+    expect(service.replaceUmlauts('')).toBe('');
+  });
 });
